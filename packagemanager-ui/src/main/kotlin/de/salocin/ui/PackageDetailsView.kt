573d7515b97bee2c4ebd78f301ef0defae124cdf
LegendSalocin <mail.salocin@gmail.com>
2021-09-14 23:46:04 +0200

    simple package details view

--- packagemanager-ui/src/main/kotlin/de/salocin/ui/PackageDetailsView.kt
+++ packagemanager-ui/src/main/kotlin/de/salocin/ui/PackageDetailsView.kt
@@ -2,8 +2,11 @@ package de.salocin.ui
 
 import de.salocin.android.AndroidPackage
 import javafx.beans.value.ObservableValue
+import javafx.scene.control.ProgressIndicator
 import javafx.scene.control.TitledPane
+import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
+import javafx.scene.text.Text
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.nio.file.Path
@@ -16,26 +19,40 @@ class PackageDetailsView(
     private val nameTextField = PackageDetailsTextField("Name")
     private val pathsList = PackageDetailsList<Path>("Paths")
 
-    private val content = VBox(nameTextField.root, pathsList.root)
+    private val content = VBox(nameTextField.root, pathsList.root).apply { isVisible = false }
+    private val contentLoader = ProgressIndicator().apply { isVisible = false }
+    private val contentNoDetails = Text("No details for selected package")
+    private val contentPane = StackPane(content, contentLoader, contentNoDetails)
 
-    override val root = TitledPane("Details", content).apply {
+    override val root = TitledPane("Details", contentPane).apply {
         isCollapsible = false
     }
 
     init {
         selectedPackage.addListener { _, _, newValue ->
-            newValue?.let {
-                if (it.detailsAvailable) {
-                    refreshDetailsJob(it)
-                }
+            content.isVisible = false
+            contentLoader.isVisible = false
+
+            if (newValue == null || !newValue.detailsAvailable) {
+                contentNoDetails.isVisible = true
+            } else {
+                contentNoDetails.isVisible = false
+                refreshDetailsJob(newValue)
             }
         }
     }
 
     private fun refreshDetailsJob(pack: AndroidPackage) {
         coroutineScope.launch {
+            contentLoader.isVisible = true
+
+            pack.refreshInstallLocation()
+
             nameTextField.text = pack.name
             pathsList.items = pack.paths.observableList()
+
+            content.isVisible = true
+            contentLoader.isVisible = false
         }
     }
 }

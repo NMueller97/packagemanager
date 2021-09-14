319b6fe31388fb85e118b4c083bfa4b482f47153
LegendSalocin <mail.salocin@gmail.com>
2021-09-14 22:42:26 +0200

    rewrite with coroutines start

---
+++ packagemanager-android/src/main/kotlin/de/salocin/android/FakeAndroidDevice.kt
@@ -0,0 +1,34 @@
+package de.salocin.android
+
+class FakeAndroidDevice(
+    override val serialNumber: String,
+    override val model: String
+) : AndroidDevice {
+
+    override val dataPackages = emptyList<AndroidPackage>()
+    override val systemPackages = emptyList<AndroidPackage>()
+    override val vendorPackages = emptyList<AndroidPackage>()
+    override val unknownPackages = emptyList<AndroidPackage>()
+
+    override suspend fun refreshPackages() {
+        // nothing to do
+    }
+
+    override fun toString(): String {
+        return model
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other === this || other is FakeAndroidDevice
+    }
+
+    override fun hashCode() = model.hashCode()
+
+    companion object {
+
+        private const val SERIAL_NUMBER = "null"
+
+        val noDevicesConnected = FakeAndroidDevice(SERIAL_NUMBER, "No devices connected")
+        val loadingDevices = FakeAndroidDevice(SERIAL_NUMBER, "Loading devices...")
+    }
+}

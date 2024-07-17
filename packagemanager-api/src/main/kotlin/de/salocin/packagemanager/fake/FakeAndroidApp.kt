65c1e55dc2a13c2c809a1ec1c68d7d994d4d8cf0
Nico Müller <mueller@cryptospace.dev>
2024-07-17 23:56:38 +0200

    fix wildcard import

--- packagemanager-api/src/main/kotlin/de/salocin/packagemanager/fake/FakeAndroidApp.kt
+++ packagemanager-api/src/main/kotlin/de/salocin/packagemanager/fake/FakeAndroidApp.kt
@@ -1,7 +1,11 @@
 package de.salocin.packagemanager.fake
 
 import de.salocin.packagemanager.ProgressObserver
-import de.salocin.packagemanager.device.*
+import de.salocin.packagemanager.device.App
+import de.salocin.packagemanager.device.AppBundle
+import de.salocin.packagemanager.device.AppType
+import de.salocin.packagemanager.device.Device
+import de.salocin.packagemanager.device.DevicePath
 import java.nio.file.Path
 
 class FakeAndroidApp(override val name: String) : App {
